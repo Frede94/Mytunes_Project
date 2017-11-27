fa83e4e712532a3aaff3d3b3233b4676e01c2cee
Frede94 <Frede94@users.noreply.github.com>
2017-11-27 09:43:23 +0100

    tilføjede BE
    tilføjede BE til projekter

---
+++ src/mytunes_project/be/Songs.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mytunes_project.be;
+
+/**
+ *
+ * @author Frederik Bærbar
+ */
+public class Songs
+{
+
+    private int ID;
+    private String title;
+    private String artistId;
+    private String categoryId;
+    private float time;
+
+    /**
+     * Get the value of time
+     *
+     * @return the value of time
+     */
+    public float getTime()
+    {
+        return time;
+    }
+
+    /**
+     * Set the value of time
+     *
+     * @param time new value of time
+     */
+    public void setTime(float time)
+    {
+        this.time = time;
+    }
+
+    /**
+     * Get the value of categoryId
+     *
+     * @return the value of categoryId
+     */
+    public String getCategoryId()
+    {
+        return categoryId;
+    }
+
+    /**
+     * Set the value of categoryId
+     *
+     * @param categoryId new value of categoryId
+     */
+    public void setCategoryId(String categoryId)
+    {
+        this.categoryId = categoryId;
+    }
+
+    /**
+     * Get the value of artistId
+     *
+     * @return the value of artistId
+     */
+    public String getArtistId()
+    {
+        return artistId;
+    }
+
+    /**
+     * Set the value of artistId
+     *
+     * @param artistId new value of artistId
+     */
+    public void setArtistId(String artistId)
+    {
+        this.artistId = artistId;
+    }
+
+    /**
+     * Get the value of title
+     *
+     * @return the value of title
+     */
+    public String getTitle()
+    {
+        return title;
+    }
+
+    /**
+     * Set the value of title
+     *
+     * @param title new value of title
+     */
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    /**
+     * Get the value of ID
+     *
+     * @return the value of ID
+     */
+    public int getID()
+    {
+        return ID;
+    }
+
+    /**
+     * Set the value of ID
+     *
+     * @param ID new value of ID
+     */
+    public void setID(int ID)
+    {
+        this.ID = ID;
+    }
+
+}
