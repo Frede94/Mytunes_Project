978d761f6b56a5fa9be809c159038ebacf7abc96
Frede94 <Frede94@users.noreply.github.com>
2017-11-28 12:45:03 +0100

    infor

--- src/mytunes_project/gui/SongInfo.java
+++ src/mytunes_project/gui/SongInfo.java
@@ -10,15 +10,13 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import mytunes_project.be.Song;
 
-
 /**
  *
  * @author morte
  */
 public class SongInfo
 {
-    
-<<<<<<< HEAD
+
 //    private ObservableList<Song> songs
 //            = FXCollections.observableArrayList();
 //    private SongManager songManager = new SongManager();
@@ -41,28 +39,4 @@ public class SongInfo
 //        songs.remove(selectedSong);
 //        songManager.remove(selectedSong);
 //    }
-=======
-    private ObservableList<Song> songs
-            = FXCollections.observableArrayList();
-
-    
-    public ObservableList<Song> getSongs() 
-    {
-        return songs;
-    }
-    
-    public void loadSongs()
-    {
-        List<Song> loadedSongs = bllManager.getAllSongs();
-        
-        songs.clear();
-        songs.addAll(loadedSongs);
-    }
-    
-    public void remove(Song selectedSong)
-    {
-        songs.remove(selectedSong);
-        bllManager.remove(selectedSong);
-    }
->>>>>>> 27b3422361d9f00c64da7571491e586b53ccf752
 }
